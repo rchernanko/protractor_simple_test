027fec59df55f807b652eb6609c2a7dcb8d9be6c
Richard Chernanko <rchernanko@hotmail.co.uk>
2016-02-09 17:05:29 +0000

    amending config file with sauce labs details

--- conf.js
+++ conf.js
@@ -1,5 +1,7 @@
 exports.config = {
     framework: 'jasmine',
-    seleniumAddress: 'http://localhost:4444/wd/hub',
+    //seleniumAddress: 'http://localhost:4444/wd/hub',
+    sauceUser: 'cims_sauce',
+    sauceKey: '8565bc35-1b13-4a57-97da-25621f016cdc',
     specs: ['spec.js']
 }
