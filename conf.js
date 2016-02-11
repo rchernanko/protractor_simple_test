ada4b8888ac5c1e6424dfce519d7c8d79636a35a
Richard Chernanko <rchernanko@hotmail.co.uk>
2016-02-11 17:39:19 +0000

    tunnel id added - once i manually start up sauce connect with the tunnel id richard, we can run the test in the cloud team enviroment

--- conf.js
+++ conf.js
@@ -3,4 +3,8 @@ exports.config = {
     sauceUser: 'cims_sauce',
     sauceKey: '529debcb-67fe-453f-9626-860fb7523b97',
     specs: ['spec.js'],
+    capabilities: {
+        'browserName': 'chrome',
+        'tunnel-identifier': 'richard',
+    }
 }
--- spec.js
+++ spec.js
@@ -1,6 +1,6 @@
 describe('Protractor Demo App', function() {
     it('should have a title', function() {
-        browser.get('http://www.sit2.marksandspencer.com/');
+        browser.get('http://pd.qa2.devops.mnscorp.net/diego-dalla-palma-lipstick/p/p22197588?image=HT_07_T23_2321L_UJ_SP14_EC_0&color=36Brick&resPDP=true');
 
         expect(browser.getTitle()).toEqual('Super Calculator');
     });
