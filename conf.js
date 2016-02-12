89d35682811ad520bdd4363215861e7b8450f663
Richard Chernanko <rchernanko@hotmail.co.uk>
2016-02-12 09:41:07 +0000

    changing tunnel name to bdd_01

--- conf.js
+++ conf.js
@@ -5,6 +5,6 @@ exports.config = {
     specs: ['spec.js'],
     capabilities: {
         'browserName': 'chrome',
-        'tunnel-identifier': 'bdd_07',
+        'tunnel-identifier': 'bdd_01',
     }
 }
