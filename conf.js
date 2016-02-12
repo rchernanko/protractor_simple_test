c022bac895a06c4aa04e8c21f79bed740f8f340b
Richard Chernanko <rchernanko@hotmail.co.uk>
2016-02-12 14:19:13 +0000

    adding node modules folder to gitignore and commenting out a few things in the conf.js

--- conf.js
+++ conf.js
@@ -1,10 +1,14 @@
 exports.config = {
     framework: 'jasmine',
-    sauceUser: 'cims_sauce',
-    sauceKey: '529debcb-67fe-453f-9626-860fb7523b97',
+
+    //in our bdd jenkins, we are using the sauce connect plugin - therefore we don't need to specify the sauce user and sauce key in here
+    //so commenting out. Have done similar thing with tunnel identifier.
+
+    //sauceUser: 'cims_sauce',
+    //sauceKey: '529debcb-67fe-453f-9626-860fb7523b97',
     specs: ['spec.js'],
     capabilities: {
         'browserName': 'chrome',
-        'tunnel-identifier': 'bdd07',
+        //'tunnel-identifier': 'bdd07',
     }
 }
