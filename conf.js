f569923bebcfd7c206cc786efd3ffff6ff5f5918
Richard Chernanko <rchernanko@hotmail.co.uk>
2016-02-12 14:55:26 +0000

    adding tunnel id

--- conf.js
+++ conf.js
@@ -5,6 +5,7 @@ exports.config = {
     sauceKey: '529debcb-67fe-453f-9626-860fb7523b97',
     capabilities: {
         'browserName': 'chrome',
+        'tunnel-identifier': 'bdd07',
     },
     baseUrl: 'http://pd.qa2.devops.mnscorp.net/diego-dalla-palma-lipstick/p/p22197588?image=HT_07_T23_2321L_UJ_SP14_EC_0&color=36Brick&resPDP=true',
     mochaOpts: {
