34286ebe16e57dd40757c222a492917aa2f475fb
Richard Chernanko <rchernanko@hotmail.co.uk>
2016-02-12 14:35:21 +0000

    adding a package json and amending spec/conf file to run with mocha

--- conf.js
+++ conf.js
@@ -1,14 +1,11 @@
 exports.config = {
-    framework: 'jasmine',
-
-    //in our bdd jenkins, we are using the sauce connect plugin - therefore we don't need to specify the sauce user and sauce key in here
-    //so commenting out. Have done similar thing with tunnel identifier.
-
-    //sauceUser: 'cims_sauce',
-    //sauceKey: '529debcb-67fe-453f-9626-860fb7523b97',
+    framework: 'mocha',
     specs: ['spec.js'],
     capabilities: {
         'browserName': 'chrome',
-        //'tunnel-identifier': 'bdd07',
+    },
+    baseUrl: 'http://pd.qa2.devops.mnscorp.net/diego-dalla-palma-lipstick/p/p22197588?image=HT_07_T23_2321L_UJ_SP14_EC_0&color=36Brick&resPDP=true',
+    mochaOpts: {
+        reporter: "spec",
     }
 }
--- spec.js
+++ spec.js
@@ -1,7 +1,12 @@
-describe('Protractor Demo App', function() {
-    it('should have a title', function() {
-        browser.get('http://pd.qa2.devops.mnscorp.net/diego-dalla-palma-lipstick/p/p22197588?image=HT_07_T23_2321L_UJ_SP14_EC_0&color=36Brick&resPDP=true');
-
-        expect(browser.getTitle()).toEqual('Super Calculator');
+describe('navigate to homepage test', function(){
+    this.timeout(25000);
+    it('should open website homepage', function(done){
+        done();
     });
+
+    before(function (done) {
+        browser.get('');
+        done();
+    });
+
 });
